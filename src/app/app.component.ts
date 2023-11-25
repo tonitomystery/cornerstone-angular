23b9c0311c9376d57e29fb0d4866e0595b42437b
Antonio <amaristany01@gmail.com>
2023-11-25 09:46:07 -0400

    feat: add events to manipulate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { CornerstoneService } from './services/cornerstone.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cornerstone-app';
+
+  constructor(private cornerstoneService: CornerstoneService) {}
+
+  handleFileSelect(evt: any) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    const files = evt.dataTransfer.files;
+
+    // this UI is only built for a single file so just dump the first one
+    let file = files[0];
+
+    const imageId = this.cornerstoneService.getImageIdFromLocalFile(file);
+
+    this.loadAndViewImage(imageId);
+  }
+
+  handleDragOver(event: any) {
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  loadAndViewImage(imageId: any) {
+    this.cornerstoneService.loadDicomFile(imageId).subscribe({
+      next: (image: any) => this.displayImage(image),
+      error: (error: any) => {
+        console.log('Error loading DICOM file:', error);
+      },
+    });
+  }
+
+  displayImage(image: any) {
+    const element = document.getElementById('dicomImage');
+
+    const isEnable = this.cornerstoneService.isEnabledElement(element);
+
+    if (!isEnable) {
+      this.cornerstoneService.enableElement(element);
+    }
+    
+    const viewport = this.cornerstoneService.getDefaultViewport(element, image);
+
+    this.cornerstoneService.displayImage(element, image, viewport);
+  }
 }
